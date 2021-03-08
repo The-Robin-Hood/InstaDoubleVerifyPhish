2f1523e6ebd1d7eb908cac72cc52cb351b7a4a33
Robinhood <32297581+The-Robin-Hood@users.noreply.github.com>
2021-03-09 00:03:25 +0530

    Update checkforverification.py

--- Core/website/checkforverification.py
+++ Core/website/checkforverification.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 while(True):
     if(os.path.exists("Verification.txt")):
+        time.sleep(1.5)
         with open("Verification.txt",'r') as H:
             print(H.read())
         break
